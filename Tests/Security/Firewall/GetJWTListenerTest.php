eaf07e0780f434ab073de0ac308cca1196dee60e
Kay Petzold <kay.petzold@db-n.com>
2022-01-14 15:18:30 +0100

    #36 add tests to project

---
+++ Tests/Security/Firewall/GetJWTListenerTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Gfreeau\Bundle\GetJWTBundle\Tests\Security\Firewall;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetJWTListenerTest
+ *
+ * @author Kay Petzold <kay.petzold@db-n.com>
+ */
+class GetJWTListenerTest extends TestCase
+{
+
+}
